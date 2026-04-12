28d50ce3731697469dd4c9be7bc374223d81e362
Christelle Xiong <mxiong5@jh.edu>
2026-04-12 16:49:15 -0400

    Update: test_dashboard.R

--- test_dashboard.R
+++ test_dashboard.R
@@ -3,7 +3,11 @@ devtools::load_all("h:/Myositis/TrajectoryDashboard")
 
 
 setwd("~/Myositis/TrajectoryDashboard")
-# Real OMOP data
+# Real OMOP data - SAFE
 con <- create_connection_from_env(".env")
+# Real OMOP data - SAFER
+con <- create_safer_connection("R.env")
+
 launch_trajectory_dashboard(con)
+
 
