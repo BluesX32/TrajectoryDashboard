5ecdb882c3e7c6791d302b89fe8ad823c1b84ac8
BluesX32 <120090034@link.cuhk.edu.cn>
2026-04-13 11:14:06 -0400

    Update: function calling

--- test_dashboard.R
+++ test_dashboard.R
@@ -4,9 +4,9 @@ devtools::load_all("h:/Myositis/TrajectoryDashboard")
 
 setwd("~/Myositis/TrajectoryDashboard")
 # Real OMOP data - SAFE
-con <- create_connection_from_env(".env")
+con <- TrajectoryDashboard::create_connection_from_env()(".env")
 # Real OMOP data - SAFER
-con <- create_safer_connection("R.env")
+con <- TrajectoryDashboard::create_safer_connection("R.env")
 
 launch_trajectory_dashboard(con)
 
